41a4dc4b275d6fe80baf296768a6e5af33654514
abdulrahim46 <abdul7716@gmail.com>
2022-05-29 09:24:42 +0530

    Initial Commit

---
+++ DeviceScan/DeviceScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceScanApp.swift
+//  DeviceScan
+//
+//  Created by Abdul Rahim on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DeviceScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
